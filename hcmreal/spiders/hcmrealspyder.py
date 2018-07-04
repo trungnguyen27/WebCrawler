fca2368874b868d0c38cbe55b82bdced7f04c3b6
trungnq <quoctrunguit@gmail.com>
2018-07-04 23:30:38 +0700

    update tracking links

--- hcmreal/spiders/hcmrealspyder.py
+++ hcmreal/spiders/hcmrealspyder.py
@@ -21,7 +21,7 @@ class ElectronicsSpider(CrawlSpider):
     name = 'hcmrealspyder'
     allowed_domains = ['www.muabannhadat.vn']
     start_urls = [
-        'http://www.muabannhadat.vn/nha-ban-3513/tp-ho-chi-minh-s59',
+        'http://www.muabannhadat.vn/can-ho-ban-chung-cu-3529/tp-ho-chi-minh-s59',
     ]
     
     print ("Setting rules")
@@ -32,6 +32,7 @@ class ElectronicsSpider(CrawlSpider):
     print("finished Setting Rules")
     def parse_item(self, response):
         item_links = response.css('.title > div > a::attr(href)').extract()
+        print('A response from %s just arrived!', response.url)
         for a in item_links:
             url = 'http://www.muabannhadat.vn'+a
             #yield PhantomJSDownloadHandler.download_request('http://www.muabannhadat.vn'+a, self.parse_detail_page)
@@ -45,28 +46,32 @@ class ElectronicsSpider(CrawlSpider):
             print("Empty URL")
             yield
 
-        print("Collecting at " + response.url)
         tables = response.css('table')
-        items = tables[0].css('tr')
+        
         realItem = HcmrealItem()
         realItem['price'] = response.css('span#MainContent_ctlDetailBox_lblPrice::text').extract()
         realItem['title'] = response.css('h1::text').extract()[0].strip()
         realItem['house_type'] = response.css('.title-detail-page > .pull-left > .seo-category-detail-page::text').extract()[0].strip()
         realItem['district'] = response.css('span#MainContent_ctlDetailBox_lblDistrict > a::text').extract()[0]
         realItem['url'] = response.url
-        print("DISTRICT " + realItem['district'])
-        for i, item in enumerate(items):
-            title = item.css('th::text').extract()[0].strip()
-            content = item.css('td>span::text').extract()
-            if content and len(content)>= 1:
-                content = content[0].strip()
-            realItem[titleDict[title]] = content
+        
+        try:
+            items = tables[0].css('tr')
+            for i, item in enumerate(items):
+                title = item.css('th::text').extract()[0].strip()
+                content = item.css('td>span::text').extract()
+                if content and len(content)>= 1:
+                    content = content[0].strip()
+                realItem[titleDict[title]] = content
+        except:
+            print('error while getting talbes')
+            
         try:
             if 'price' in realItem and 'area' in realItem and 'bedrooms' in realItem:
-                yield realItem
+                if len(realItem['price']) > 0 and len(realItem['area']) > 0 and len(realItem['bedrooms'])>0:
+                    yield realItem
         except:
             print("error")
-        yield realItem
 
       
 
